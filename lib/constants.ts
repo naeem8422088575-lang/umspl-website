54e12585bee36c320e87c735377ad83294c14941
naeem8422088575-lang <naeem8422088575@gmail.com>
2026-06-16 13:35:45 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const COMPANY_METADATA = {
   abbreviation: 'UMSPL',
   officialName: 'Unimar Marine Services Private Limited',
   email: 'crewing@unimarmarine.com',
-  phones: ['+91 22 2812 0000', '+91 98 0000 0000'],
+  phones: ['+91 9768285965', '+91 22 2812 0000'],
   address: 'Opposite Mira Road Railway Station Layout, Mira Road East, Mumbai, Maharashtra 401107, India',
   seo: { 
     url: 'https://unimarmarine.com', 
