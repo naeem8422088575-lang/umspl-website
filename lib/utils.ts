6f286efa7d62c98b895f9fdcbf77af422c1ee8bd
naeem8422088575-lang <naeem8422088575@gmail.com>
2026-06-18 13:10:46 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,21 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { COMPANY_METADATA } from "./constants";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function generateSecureWhatsAppLink(context: string, rank?: string): string {
-  const baseMessage = rank 
-    ? Hello ${COMPANY_METADATA.abbreviation} Recruitment, I am inquiring about the ${rank} position.
-    : Hello ${COMPANY_METADATA.abbreviation} Recruitment, I am reaching out regarding maritime opportunities.;
-  
-  const safePayload = encodeURIComponent(${baseMessage} Context: ${context});
-  const safeNumber = COMPANY_METADATA.whatsapp.replace(/[^0-9]/g, '');
-  
-  return https://wa.me/${safeNumber}?text=${safePayload};
+  var baseMessage;
+  if (rank) {
+    baseMessage = "Hello " + COMPANY_METADATA.abbreviation + " Recruitment, I am inquiring about the " + rank + " position.";
+  } else {
+    baseMessage = "Hello " + COMPANY_METADATA.abbreviation + " Recruitment, I am reaching out regarding maritime opportunities.";
+  }
+
+  var safePayload = encodeURIComponent(baseMessage + " Context: " + context);
+  var safeNumber = COMPANY_METADATA.whatsapp.replace(/[^0-9]/g, '');
+
+  return "https://wa.me/" + safeNumber + "?text=" + safePayload;
 }
